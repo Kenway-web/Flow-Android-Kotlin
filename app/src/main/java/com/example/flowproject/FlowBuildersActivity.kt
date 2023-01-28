97054f006d8a69f47fb37971edccdc5b721123b7
Shoaib Ali <shoaibali26021999@gmail.com>
2023-01-28 19:53:13 +0530

    Events

--- app/src/main/java/com/example/flowproject/FlowBuildersActivity.kt
+++ app/src/main/java/com/example/flowproject/FlowBuildersActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 
 import kotlinx.coroutines.launch
@@ -56,10 +59,34 @@ class FlowBuildersActivity : AppCompatActivity() {
 //            }
 //        }
 
+        GlobalScope.launch(Dispatchers.Main) {
+            producer()
+                .onStart {
+                    Log.d("TAG","Starting out")
+                    emit(-100)
+                }
+                .onCompletion {
+                    Log.d("TAG","Completed")
+                }
+                .onEach {
+                    Log.d("TAG","About to Emit = $it")
+                }
+                .collect{
+                Log.d("TAG","${it.toString()}")
+            }
+        }
 
 
+    }
 
 
-
+    private fun producer():Flow<Int>{
+        return flow<Int>{
+            val list=listOf(1,2,3,4,5)
+            list.forEach{
+                delay(1000)
+                emit(it)
+            }
+        }
     }
 }
