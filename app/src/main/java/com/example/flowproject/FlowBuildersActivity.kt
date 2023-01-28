7ac1c7afb217216643799e9c6e15690affa1c320
Shoaib Ali <shoaibali26021999@gmail.com>
2023-01-28 19:05:48 +0530

    Flow builders

---
+++ app/src/main/java/com/example/flowproject/FlowBuildersActivity.kt
@@ -0,0 +1,65 @@
+package com.example.flowproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.*
+
+import kotlinx.coroutines.launch
+
+class FlowBuildersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flow_builders)
+
+//        lifecycleScope.launch {
+//            flowOf(4,3,2,1,5,6).collect{
+//                Log.d("FlowBuilders", "flowOf():$it")
+//            }
+//        }
+//
+//
+//        lifecycleScope.launch {
+//            flowOf("Shoaib","Ali","Bruce","Wayne").collect{
+//                Log.d("FlowBuilder", "flowOf():$it")
+//            }
+//        }
+
+
+//        lifecycleScope.launch {
+//            (1..5).asFlow().collect{
+//                Log.d("Flow Builders","asFlow : $it")
+//            }
+//        }
+
+
+//        lifecycleScope.launch {
+//         flow {
+//             (0..10).forEach{
+//                 emit(it)
+//             }
+//         }.collect{
+//             Log.e("Flow Builders","flow : $it")
+//         }
+//        }
+//
+
+
+//        lifecycleScope.launch{
+//            channelFlow {
+//                (0..10).forEach {
+//                    send(it)
+//                }
+//            }.collect{
+//                Log.d("Flow Builders","channelFlow: $it")
+//            }
+//        }
+
+
+
+
+
+
+    }
+}
